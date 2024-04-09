d17062281815d9c5fe86b9023166080f5173101b
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-09 17:48:14 -0500

    Update Card component to increment bet count

--- unit7Lab/client/src/components/Card.js
+++ unit7Lab/client/src/components/Card.js
@@ -3,12 +3,18 @@ import { useState } from 'react'
 import './Card.css'
 import more from './more.png'
 import { Link } from 'react-router-dom'
+import { supabase } from '../client'
 
 
 const Card = (props) =>  {
 
   const [count, setCount] = useState(0)
-  const updateCount = () => {
+
+  const updateCount = async () => {
+    await supabase
+      .from('Posts')
+      .update({betCount: count + 1})
+      .eq('id', props.id)
     setCount((count) => count + 1);
   }
 
