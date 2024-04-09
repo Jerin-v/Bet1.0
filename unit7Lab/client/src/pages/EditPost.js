d84e46dfc7003c486a74e1845965e9657db059dc
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-04-09 17:09:30 -0500

    Add updatePost function to EditPost component

--- unit7Lab/client/src/pages/EditPost.js
+++ unit7Lab/client/src/pages/EditPost.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { supabase } from '../client';
 import './EditPost.css'
 
 const EditPost = ({data}) => {
 
     const {id} = useParams();
     const [post, setPost] = useState({id: null, title: "", author: "", description: ""});
+
+    const updatePost = async (event) => {
+        event.preventDefault();
+        await supabase
+            .from('Posts')
+            .update({title: post.title, author: post.author, description: post.description})
+            .eq('id', id)
+
+        window.location = "/"
+    }
 
     const handleChange = (event) => {
         const {name, value} = event.target;
@@ -29,10 +40,10 @@ const EditPost = ({data}) => {
                 <br/>
 
                 <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" value={post.description} onChange={handleChange} >
+                <textarea rows="5" cols="50" id="description" name="description" value={post.description} onChange={handleChange} >
                 </textarea>
                 <br/>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={updatePost} />
                 <button className="deleteButton">Delete</button>
             </form>
         </div>
